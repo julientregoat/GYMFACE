1079ad238d64a3a73ddfceae61420920b19ea6f0
github username <github email address>
2018-02-28 18:17:40 -0500

    some initial things

---
+++ gymface-front-end/src/GymFaceContainer.js
@@ -0,0 +1,15 @@
+import React from "react"
+import CalendarContainer from "./CalendarContainer"
+
+class GymFaceContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>GymFaceContainer</h1>
+        <CalendarContainer/>
+      </div>
+    );
+  }
+}
+
+export default GymFaceContainer;

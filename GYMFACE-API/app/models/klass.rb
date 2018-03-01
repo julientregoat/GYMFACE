f3c0c31acf90715b10482322d50ed4576cd8fc13
julientregoat <julienctregoat@gmail.com>
2018-03-01 15:51:28 -0500

    scraping classes successfully

--- GYMFACE-API/app/models/klass.rb
+++ GYMFACE-API/app/models/klass.rb
@@ -5,14 +5,33 @@ class Klass < ApplicationRecord
   has_many :user_klasses
   has_many :users, through: :user_klasses
 
-  def self.fetchClasses(location_id)
-    url = 'https://www.equinox.com/classschedule?clubs=' + location_id.to_s
+  def self.fetchKlasses(location_id, date = Date.today.strftime("%Y-%m-%d"))
+    # needs YYYY-MM-DD format
+
+    url = 'https://www.equinox.com/classschedule?clubs=' + location_id.to_s + "&date=" + date
+
     browser = Watir::Browser.new(:chrome)
     browser.goto(url)
     sleep 2
     page = Nokogiri::HTML(browser.html)
-    # search-result-row is the class of every item, rendered in JS
-    byebug
+
+    # .search-result-row is the class of every item
+    # .search-result-row.class-info provides the class info
+    # => h2 has the class title
+    # => .icon-time span has the time
+    # => .icon-trainer span has the trainer
+    # => .icon-marker span has the location
+
+    klasses = page.css('.search-result-row .class-info')
+    klasses.each do |klass|
+      puts name = klass.css('h2').text.strip #title
+      time = klass.css('.icon-time').text.strip.split(" - ")
+      puts start = date + " " + time.first
+      puts ending = date + " " + time.last
+      puts trainer = klass.css('.icon-trainer').text.strip #trainer
+      byebug
+      # Klass.find_or_create_by(name: name, instructor: trainer, start_time: start, end_time: ending)
+    end
     browser.close
   end
 end
--- GYMFACE-API/db/schema.rb
+++ GYMFACE-API/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301154258) do
+ActiveRecord::Schema.define(version: 20180301205033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180301154258) do
   create_table "klasses", force: :cascade do |t|
     t.string "name"
     t.string "instructor"
-    t.date "date"
     t.time "start_time"
     t.time "end_time"
     t.datetime "created_at", null: false

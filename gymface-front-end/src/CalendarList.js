f7852889ca6951338181fc4692c4b234acd355ba
julientregoat <julienctregoat@gmail.com>
2018-03-02 13:04:55 -0500

    mirroring changes

--- gymface-front-end/src/CalendarList.js
+++ gymface-front-end/src/CalendarList.js
@@ -1,15 +1,14 @@
 import React from "react"
 import CalendarItem from "./CalendarItem"
 
-const CalendarList (props) => {
+const CalendarList = (props) => {
 //map through classlist from fetch 
-    return (
-      <div classname="mini-border">
-        <h1>CalendarList</h1>
-        <CalendarItem/>
-      </div>
-    );
-  }
+  return (
+    <div classname="mini-border">
+      <h1>CalendarList</h1>
+      <CalendarItem/>
+    </div>
+  );
 }
 
 export default CalendarList;

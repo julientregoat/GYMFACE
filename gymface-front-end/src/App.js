ecda961a57fa02d2662e8a7a68c6aca92aa89fc9
julientregoat <julienctregoat@gmail.com>
2018-03-02 13:20:02 -0500

    doing some layout scaffolding for the front end

--- gymface-front-end/src/App.js
+++ gymface-front-end/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
+import Dashboard from './Dashboard'
+import Header from './Header'
+
 import './App.css';
-import GymFaceContainer from "./GymFaceContainer"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <GymFaceContainer/>
+        <Header />
+        <Dashboard />
       </div>
     );
   }
---
+++ gymface-front-end/src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CalendarContainer from "./CalendarContainer"
+
+const Dashboard = ({}) => (
+  <div>
+    <CalendarContainer />
+  </div>
+);
+
+export default Dashboard;

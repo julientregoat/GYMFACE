28bcf5f2945b92035fd43ec9f7f8025ebe3d6713
julientregoat <julienctregoat@gmail.com>
2018-03-02 19:50:40 -0500

    routing + routing placeholders

--- gymface-front-end/src/Header.js
+++ gymface-front-end/src/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
 const Header = () => (
   <div id="header">
     <h1>GymFace Header</h1>
     <Menu inverted>
-      <Menu.Item content="Dashboard"/>
-      <Menu.Item content="View Classes"/>
-      <Menu.Item content="View Restaurants"/>
-      <Menu.Item position="right" content="Your Account"/>
+      <Link to="/"><Menu.Item content="Dashboard"/></Link>
+      <Link to="/classes"><Menu.Item content="View Classes"/></Link>
+      <Link to="/"><Menu.Item content="View Restaurants"/></Link>
+      <Link to="/"><Menu.Item position="right" content="Your Account"/></Link>
     </Menu>
+    
   </div>
 );
 

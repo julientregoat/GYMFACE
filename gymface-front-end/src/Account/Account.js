655d67a2ce7fadde5c818bbabf2253d8f860d9a4
julientregoat <julienctregoat@gmail.com>
2018-03-03 14:43:28 -0500

    was playing around with image capture on the account page - is now capturing photos! now gotta do login page and capture a pic to store for our gallery

--- gymface-front-end/src/Account/Account.js
+++ gymface-front-end/src/Account/Account.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import UserInfo from './UserInfo'
 
 import { Grid } from 'semantic-ui-react'
+import Webcam from 'react-webcam'
 
 class Account extends Component {
 
   state = {
-    user: {}
+    user: {},
+    capturedImage: ""
   }
 
   componentDidMount(){
@@ -15,11 +17,26 @@ class Account extends Component {
     .then(res => res.json())
     .then(user => this.setState({user: user}))
   }
+  
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  }
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    this.setState({capturedImage: imageSrc})
+  };
 
   render() {
     return (
       <Grid centered columns={2}>
-        <UserInfo user={this.state.user}/>
+        <Grid.Row><UserInfo user={this.state.user}/>
+        <button onClick={this.capture}>Capture photo</button></Grid.Row>
+        <Grid.Row><Webcam 
+        audio={false} 
+        ref={this.setRef}
+        screenshotFormat="image/jpeg"/>
+        <img src={this.state.capturedImage}/></Grid.Row>
       </Grid>
     );
   }
